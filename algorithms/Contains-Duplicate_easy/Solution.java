5d2fdfb90f2415ac6f1c32127c9271a5c7dda710
wanglei828 <bestwanglei@gmail.com>
2016-06-23 21:43:23 -0700

    Create Solution.java

---
+++ algorithms/Contains-Duplicate_easy/Solution.java
@@ -0,0 +1,21 @@
+/*
+Given an array of integers, find if the array contains any duplicates. 
+Your function should return true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
+*/
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+      if(nums==null || nums.length==0)
+          return false;
+   
+      HashSet<Integer> set = new HashSet<Integer>();
+      for(int i: nums){
+          if(!set.add(i)){
+              return true;
+          }
+      }
+   
+      return false;
+    }
+}
