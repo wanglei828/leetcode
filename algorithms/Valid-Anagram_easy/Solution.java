bd777911b830dc9d910c708e6a8b3d5c5c6d9c44
wanglei828 <bestwanglei@gmail.com>
2016-06-23 21:37:54 -0700

    Create Solution.java

---
+++ algorithms/Valid-Anagram_easy/Solution.java
@@ -0,0 +1,26 @@
+/*
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+*/
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null && t != null) return false;
+        if(s != null && t == null) return false;
+        if(s == null && t == null) return true;
+        if(s.length() != t.length()) return false;
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        String news = new String(sArr);
+        String newt = new String(tArr);
+        return news.equals(newt);
+    }
+}
