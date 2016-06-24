7dc8ca551f4e0b7a683bb6bf1c4542771d31518f
wanglei828 <bestwanglei@gmail.com>
2016-06-24 10:58:21 -0700

    Create Solution.java

---
+++ algorithms/Valid-Parentheses_easy/Solution.java
@@ -0,0 +1,39 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*/
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            if (a == '(' || a == '{' || a == '[') {
+                stack.push(s.charAt(i));
+            } else if (a == ')' || a == '}' || a == ']') {
+                if(stack.empty()){
+                    return false;
+                }
+                switch(a) {
+                    case ')':
+                        if(stack.pop() != '(') {
+                            return false;
+                        } break;
+                    case '}':
+                        if(stack.pop() != '{') {
+                            return false;
+                        } break;
+                    case ']':
+                        if(stack.pop() != '[') {
+                            return false;
+                        } break;
+                    default: break;
+                }
+            } else {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}
