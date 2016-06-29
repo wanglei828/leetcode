d8101d05bc9f629ff7e0df3cb6b771f232774451
Lei <bestwanglei@gmail.com>
2016-06-28 22:16:32 -0700

    Create Solution2.java

---
+++ algorithms/Single-Number_medium/Solution2.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            if(set.contains(nums[i])) {
+                set.remove(nums[i]);
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return set.iterator().next();
+    }
+}
