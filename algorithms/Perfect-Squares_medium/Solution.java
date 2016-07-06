33790d1a3cb1cfc64bf21d53814c4fd09ac94a1d
Lei <bestwanglei@gmail.com>
2016-07-06 16:38:42 -0700

    Create Solution.java

---
+++ algorithms/Perfect-Squares_medium/Solution.java
@@ -0,0 +1,28 @@
+/*
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+*/
+
+public class Solution {
+    public int numSquares(int n) {
+        if(n<=0) return 0;
+        int maxS = (int)Math.sqrt(n);
+        if(n == maxS*maxS) {
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            dp[i] = i;
+            int sqrt = (int)Math.sqrt(i);
+            for(int j=1; j<=sqrt; j++) {
+                int min = 1+ dp[i-j*j];
+                if(dp[i] > min) {
+                    dp[i] = min;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
