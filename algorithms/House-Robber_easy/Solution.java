a9dcb95d495f6975dc09c4f529c7ce7e463dbb66
Lei <bestwanglei@gmail.com>
2016-07-13 19:02:00 -0700

    Update Solution.java

--- algorithms/House-Robber_easy/Solution.java
+++ algorithms/House-Robber_easy/Solution.java
@@ -11,11 +11,9 @@ determine the maximum amount of money you can rob tonight without alerting the p
 
 public class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
+        if(nums == null || nums.length == 0) return 0;
         if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0], nums[1]);
         int[] mon = new int[nums.length];
-        Arrays.fill(mon, 0);
         mon[0] = nums[0];
         mon[1] = Math.max(nums[0], nums[1]);
         for(int i = 2; i<nums.length; i++){
