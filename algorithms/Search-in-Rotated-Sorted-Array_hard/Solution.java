0eaee9d3ec2bcfbfc9f81401ce37bb243575fd4e
Lei <bestwanglei@gmail.com>
2016-07-13 17:17:17 -0700

    Update Solution.java

--- algorithms/Search-in-Rotated-Sorted-Array_hard/Solution.java
+++ algorithms/Search-in-Rotated-Sorted-Array_hard/Solution.java
@@ -12,44 +12,29 @@ public class Solution {
     public int search(int[] nums, int target) {
         if(nums == null || nums.length == 0) return -1;
         int n = nums.length;
-        int i = findsmall(nums, 0, n-1);
-        if(target >= nums[i] && target <= nums[n-1]) {
-            return bsearch(nums, i, n-1, target);
-        }
-        if(i>0 && target<= nums[i-1] && target >= nums[0]) {
-            return bsearch(nums, 0, i-1, target);
-        }
-        return -1;
-    }
-    
-    private int findsmall(int[]nums, int h, int t) {
-        if(nums[h] < nums[t]) return h;
-        while(h<t) {
-            int m = h+(t-h)/2;
-            if(nums[m] < nums[t]) {
-                t = m;
-            } else if(nums[m] > nums[h]) {
-                h = m+1;
-            }
-            if(h+1 == t) {
-                return (nums[h] < nums[t])? h : t;
-            }
-        }
-        return h;
-    }
-    
-    private int bsearch(int[] nums, int h, int t, int target) {
+        int h = 0;
+        int t = n-1;
         while(h<=t) {
-            int m = h+(t-h)/2;
-            if(nums[m] == target) {
+            int m = h + (t-h)/2;
+            if(target == nums[m]) {
                 return m;
-            } else if(nums[m] > target) {
-                t = m-1;
-            } else {
-                h = m+1;
+            }
+            if(nums[m] >= nums[h]) {
+                if(target<nums[m] && target >= nums[h]) {
+                    t = m-1;
+                } else {
+                    h = m+1;
+                }
+            }
+            if(nums[m] <= nums[t]) {
+                if(target > nums[m] && target <= nums[t]) {
+                    h = m+1;
+                } else {
+                    t = m-1;
+                }
             }
         }
+
         return -1;
     }
-
 }
