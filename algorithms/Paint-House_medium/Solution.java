084813ee17facba64305b0bf19e045586e15d86b
Lei <bestwanglei@gmail.com>
2016-07-13 19:35:44 -0700

    Update Solution.java

--- algorithms/Paint-House_medium/Solution.java
+++ algorithms/Paint-House_medium/Solution.java
@@ -15,17 +15,11 @@ public class Solution {
     public int minCost(int[][] costs) {
         if(costs == null || costs.length == 0 || costs[0].length == 0) return 0;
         int n = costs.length;
-        int[] dp0 = new int[n];
-        int[] dp1 = new int[n];
-        int[] dp2 = new int[n];
-        dp0[0] = costs[0][0];
-        dp1[0] = costs[0][1];
-        dp2[0] = costs[0][2];
         for(int i=1; i<n; i++) {
-            dp0[i] = Math.min(dp1[i-1], dp2[i-1]) + costs[i][0];
-            dp1[i] = Math.min(dp0[i-1], dp2[i-1]) + costs[i][1];
-            dp2[i] = Math.min(dp0[i-1], dp1[i-1]) + costs[i][2];
+            costs[i][0] += Math.min(costs[i-1][1], costs[i-1][2]); 
+            costs[i][1] += Math.min(costs[i-1][0], costs[i-1][2]);
+            costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1]);
         }
-        return Math.min(dp0[n-1], Math.min(dp1[n-1], dp2[n-1]));
+        return Math.min(costs[n-1][0], Math.min(costs[n-1][1], costs[n-1][2]));
     }
 }
