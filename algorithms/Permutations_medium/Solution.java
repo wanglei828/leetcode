1a79793a5b2b1c3e0cbecdb30eb12a7064bc2cfa
Lei <bestwanglei@gmail.com>
2016-07-13 17:24:24 -0700

    Update Solution.java

--- algorithms/Permutations_medium/Solution.java
+++ algorithms/Permutations_medium/Solution.java
@@ -17,23 +17,23 @@ public class Solution {
     List<List<Integer>> res = new ArrayList<List<Integer>>();
     public List<List<Integer>> permute(int[] nums) {
         if(nums == null || nums.length == 0) return res;
-        helper(null, nums);
+        List<Integer> list = new ArrayList<Integer>();
+        helper(list, nums);
         return res;
     }
     private void helper(List<Integer> list, int[] nums){
-        if(list != null && list.size()==nums.length) {
+        if(list.size()==nums.length) {
             List<Integer> copy = new ArrayList<Integer>();
             for(Integer i:list) {
-                copy.add(i);
+                copy.add(nums[i]);
             }
             res.add(copy);
             return;
         }
-        list = (list == null)? new ArrayList<Integer>() : list;
         int size = list.size();
         for(int i=0; i<nums.length; i++) {
-            if(list.contains(nums[i])) continue;
-            list.add(nums[i]);
+            if(list.contains(i)) continue;
+            list.add(i);
             helper(list, nums);
             list.remove(size);
         }
