81af667fbb3e8a1f0695aa56e76335d99266afa7
Lei <bestwanglei@gmail.com>
2016-07-14 17:31:43 -0700

    Update NumMatrix.java

--- algorithms/Range-Sum-Query-2D---Immutable_medium/NumMatrix.java
+++ algorithms/Range-Sum-Query-2D---Immutable_medium/NumMatrix.java
@@ -22,57 +22,30 @@ You may assume that row1 ≤ row2 and col1 ≤ col2.
 */
 
 public class NumMatrix {
-    private int[][] data;
     private int[][] sum;
+    int m;
+    int n;
     public NumMatrix(int[][] matrix) {
         if(matrix.length==0) return;
         if(matrix[0].length == 0)return;
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        data = new int[rows][cols];
-        sum = new int[rows][cols];
-        for(int i = 0; i< rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                data[i][j] = matrix[i][j];
-                if(i == 0 ) {
-                    if(j==0) {
-                        sum[i][j] = matrix[i][j];
-                    } else {
-                        sum[i][j] = sum[i][j-1] + matrix[i][j];
-                    }
-                } else {
-                    if(j==0) {
-                        sum[i][j] = sum[i-1][j] + matrix[i][j];
-                    } else {
-                        sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i][j];
-                    }
-                }
+        m = matrix.length;
+        n = matrix[0].length;
+        sum = new int[m+1][n+1];
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                sum[i][j] = sum[i][j-1] + sum[i-1][j] - sum[i-1][j-1] + matrix[i-1][j-1]; 
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int c1=0, c2=0, c3=0;
-        if(row1==0 || col1==0) {
-            c1 = 0;
-        } else {
-            c1 = sum[row1-1][col1-1];
-        }
-        if(row1 == 0) {
-            c2 = 0;
-        } else {
-            c2 = sum[row1-1][col2];
-        }
-        if(col1==0) {
-            c3 = 0;
-        } else {
-            c3 = sum[row2][col1-1];
-        }
-        return sum[row2][col2]-c1 - (c2-c1) - (c3-c1);
-        
+        return getSum(row2, col2) - getSum(row1-1, col2) - getSum(row2, col1-1) + getSum(row1-1, col1-1);
+    }
+    
+    private int getSum(int i, int j) {
+        return sum[i+1][j+1];
     }
 }
-
 
 // Your NumMatrix object will be instantiated and called as such:
 // NumMatrix numMatrix = new NumMatrix(matrix);
