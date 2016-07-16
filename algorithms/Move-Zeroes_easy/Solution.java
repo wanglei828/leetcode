b224c056417415d0d8d90876f0867b7e3db0a4f7
Lei <bestwanglei@gmail.com>
2016-07-16 14:00:01 -0700

    Update Solution.java

--- algorithms/Move-Zeroes_easy/Solution.java
+++ algorithms/Move-Zeroes_easy/Solution.java
@@ -12,15 +12,13 @@ public class Solution {
     public void moveZeroes(int[] nums) {
         if(nums == null || nums.length <2) return;
         int pos = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[pos] == 0) {
-                if(nums[i] != 0) {
-                    int tmp = nums[i];
-                    nums[i] = nums[pos];
-                    nums[pos] = tmp;
-                    pos++;
+        int n = nums.length;
+        for(int i=0; i<n; i++) {
+            if(nums[i] != 0) {
+                if(nums[pos] == 0) {
+                    nums[pos] = nums[i];
+                    nums[i] = 0;
                 }
-            } else {
                 pos++;
             }
         }
