d280cbe090e9dbe8f1637d07feebc7a0ac6156f4
Lei <bestwanglei@gmail.com>
2016-07-17 10:21:52 -0700

    Update Solution2.java

--- algorithms/Closest-Binary-Search-Tree-Value_easy/Solution2.java
+++ algorithms/Closest-Binary-Search-Tree-Value_easy/Solution2.java
@@ -15,24 +15,17 @@ You are guaranteed to have only one unique value in the BST that is closest to t
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 public class Solution {
-    int res = -1;
-    double min = Double.POSITIVE_INFINITY;
+
     public int closestValue(TreeNode root, double target) {
-        if((double)(root.val) == target) {
-            min = 0;
-            res = root.val;
-            return res;
+        int res = root.val;
+        while(root != null) {
+            if(Math.abs((double)root.val - target) < Math.abs((double)res - target)) {
+                res = root.val;
+            }
+            root = (target > (double)root.val)? root.right : root.left;
         }
-        if(min > Math.abs((double)(root.val) - target)) {
-            min = Math.abs((double)(root.val) - target);
-            res = root.val;
-        }
-        if(target > (double)root.val && root.right != null) {
-            closestValue(root.right, target);
-        } else if(target < (double)root.val && root.left != null) {
-            closestValue(root.left, target);
-        } 
         return res;
     }
 }
