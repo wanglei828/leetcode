fb48f10508f49abe39b198524040105b4bd2f7a6
Lei <bestwanglei@gmail.com>
2016-07-19 11:21:31 -0700

    Update Solution.java

--- algorithms/Power-of-Three_easy/Solution.java
+++ algorithms/Power-of-Three_easy/Solution.java
@@ -7,15 +7,9 @@ Could you do it without using any loop / recursion?
 
 public class Solution {
     public boolean isPowerOfThree(int n) {
-        if ( n < 0) return false;
-        if (n == 1) return true;
-        int i = n/3;
-        int j = n%3;
-        while (i > 0 && j == 0) {
-            if (i == 1) return true;
-            j = i%3;
-            i = i/3;
+        if(n>1) {
+            while(n%3 == 0) n = n/3;
         }
-        return false;
+        return n == 1;
     }
 }
