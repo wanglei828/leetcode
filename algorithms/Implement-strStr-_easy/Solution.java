5e8d1eb0aeaad6a7cbdd9e0899be849631b043bf
Lei <bestwanglei@gmail.com>
2016-07-19 15:45:09 -0700

    Update Solution.java

--- algorithms/Implement-strStr-_easy/Solution.java
+++ algorithms/Implement-strStr-_easy/Solution.java
@@ -8,27 +8,12 @@ public class Solution {
     public int strStr(String haystack, String needle) {
         if(haystack == null || needle == null) return -1;
         if(needle.length() == 0) return 0;
-        if(haystack.length() == needle.length()) {
-            if(needle.equals(haystack)) {
-                return 0;
-            } else {
-                return -1;
+        for(int i = 0; ; i++) {
+            for(int j = 0; ;j++) {
+                if (j == needle.length()) return i;
+                if(i+j == haystack.length()) return -1;
+                if(haystack.charAt(i+j) != needle.charAt(j)) break;
             }
         }
-        if(haystack.length() < needle.length()) return -1;
-        for(int i = 0; i < haystack.length(); i++) {
-            for(int j = 0; j < needle.length();j++) {
-                if(i+j == haystack.length()) {
-                    break;
-                }
-                if(haystack.charAt(i+j) != needle.charAt(j)) {
-                    break;
-                }
-                if (j == needle.length()-1) {
-                    return i;
-                }
-            }
-        }
-        return -1;
     }
 }
