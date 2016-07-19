545a9ad28924cb7138430dbf3c1040e5028f4289
Lei <bestwanglei@gmail.com>
2016-07-19 10:05:41 -0700

    Update Logger.java

--- algorithms/Logger-Rate-Limiter_easy/Logger.java
+++ algorithms/Logger-Rate-Limiter_easy/Logger.java
@@ -35,19 +35,14 @@ public class Logger {
         map = new HashMap<String, Integer>();
     }
     
-    /** Returns true if the message should be printed in the given timestamp, otherwise returns false. The timestamp is in seconds granularity. */
+    /** Returns true if the message should be printed in the given timestamp, 
+    otherwise returns false. The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if(!map.containsKey(message)) {
-            map.put(message, timestamp);
-            return true;
-        } else {
-            if((timestamp - map.get(message)) >= 10) {
-                map.put(message, timestamp);
-                return true;
-            } else {
-                return false;
-            }
+        if(map.containsKey(message) && timestamp-map.get(message) < 10) {
+            return false;
         }
+        map.put(message, timestamp);
+        return true;
     }
 }
 
