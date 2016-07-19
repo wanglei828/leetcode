5189b2a0ff908125760ebd794218d5a4460faf31
Lei <bestwanglei@gmail.com>
2016-07-19 12:19:07 -0700

    Update Solution.java

--- algorithms/Palindrome-Number_easy/Solution.java
+++ algorithms/Palindrome-Number_easy/Solution.java
@@ -5,13 +5,13 @@ Determine whether an integer is a palindrome. Do this without extra space.
 public class Solution {
     public boolean isPalindrome(int x) {
         if(x<0) return false;
-        if(x==0) return true;
-        int org = x;
-        int newx = 0;
+        if(x>=0 && x<=9) return true;
+        long y = (long)x;
+        long z = 0;
         while(x>0) {
-            newx = newx*10 + x%10;
+            z = z*10 + x%10;
             x = x/10;
         }
-        return newx == org;
+        return y == z;
     }
 }
