84438e9bbf6a859b37db8ce2ccc8d697104e9e66
Lei <bestwanglei@gmail.com>
2016-07-19 16:03:47 -0700

    Update Solution.java

--- algorithms/Reverse-Integer_easy/Solution.java
+++ algorithms/Reverse-Integer_easy/Solution.java
@@ -11,7 +11,9 @@ Here are some good questions to ask before coding. Bonus points for you if you h
 
 If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
 
-Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
+Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, 
+then the reverse of 1000000003 overflows. 
+How should you handle such cases?
 
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
