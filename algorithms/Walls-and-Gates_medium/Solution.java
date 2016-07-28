e2db7f8688df37084f8a4a9be2b0978e2d23d067
Lei <bestwanglei@gmail.com>
2016-07-27 19:43:00 -0700

    Update Solution.java

--- algorithms/Walls-and-Gates_medium/Solution.java
+++ algorithms/Walls-and-Gates_medium/Solution.java
@@ -23,29 +23,25 @@ public class Solution {
     public void wallsAndGates(int[][] rooms) {
         if(rooms == null || rooms.length == 0 || rooms[0].length == 0) return;
         int[][] dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        helper(rooms, 0, dir);
-    }
-    
-    private void helper(int[][] rooms, int tag, int[][] dir) {
-        int m = rooms.length;
-        int n = rooms[0].length;
-        int flag = 0;
+        Queue<int[]> q = new LinkedList<int[]>();
+        int m = rooms.length, n=rooms[0].length;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(rooms[i][j] == tag) {
-                    for(int k=0; k<4; k++) {
-                        int x = i + dir[k][0];
-                        int y = j + dir[k][1];
-                        if(x>=0 && x <m && y>=0 && y<n && rooms[x][y] > tag+1) {
-                            rooms[x][y] = tag + 1;
-                            flag = 1;
-                        }
-                    }
-
+                if(rooms[i][j] == 0) {
+                    q.add(new int[]{i, j});
                 }
             }
         }
-        if(flag == 0) return;
-        helper(rooms, tag+1, dir);
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            for(int i=0; i<4; i++) {
+                int x = cur[0] + dir[i][0];
+                int y = cur[1] + dir[i][1];
+                if(x>=0 && x<m && y>=0 && y<n && rooms[x][y] == Integer.MAX_VALUE) {
+                    rooms[x][y] = rooms[cur[0]][cur[1]] + 1;
+                    q.add(new int[]{x, y});
+                }
+            }
+        }
     }
 }
