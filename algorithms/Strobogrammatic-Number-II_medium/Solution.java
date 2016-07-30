e45a4cd9cc47a60baa261a38c729a0c87af4f3c7
Lei <bestwanglei@gmail.com>
2016-07-29 19:43:35 -0700

    Update Solution.java

--- algorithms/Strobogrammatic-Number-II_medium/Solution.java
+++ algorithms/Strobogrammatic-Number-II_medium/Solution.java
@@ -8,38 +8,26 @@ Given n = 2, return ["11","69","88","96"].
 */
 
 public class Solution {
-    List<String> res = new ArrayList<String>();
+    private char[][] pair = {{'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
+    private List<String> res = new ArrayList<String>();
     public List<String> findStrobogrammatic(int n) {
         if(n == 0) return res;
-        char[] arr = new char[n];
-        helper(arr, 0, n);
+        dfs(new char[n], 0, n-1);
         return res;
     }
     
-    private void helper(char[] arr, int start, int n) {
-        int half = (n%2 == 0)? n/2 : n/2+1;
-        if(start == half) {
-            res.add(new String(arr));
+    private void dfs(char[] c, int h, int t) {
+        if(h>t) {
+            String str = new String(c);
+            res.add(str);
             return;
         }
-        char[] set = {'0', '1', '8', '6', '9'};
-        int j = (start == 0)? 1 : 0;
-        if(n%2 == 0 || start < half-1) {
-            for(; j<set.length; j++) {
-                arr[start] = set[j];
-                if(arr[start] == '6') {
-                    arr[n-1-start] = '9';
-                } else if(arr[start] == '9') {
-                    arr[n-1-start] = '6';
-                } else {
-                    arr[n-1-start] = arr[start];
-                }
-                helper(arr, start+1, n);
-            }
-        } else {
-            for(j=0; j<3; j++) {
-                arr[start] = set[j];
-                helper(arr, start+1, n);
+        for(char[] p: pair) {
+            c[h] = p[0];
+            c[t] = p[1];
+            if(c.length > 1 && c[0] == '0') continue;
+            if(h < t || h == t && p[0] == p[1]) {
+                dfs(c, h+1, t-1);
             }
         }
     }
