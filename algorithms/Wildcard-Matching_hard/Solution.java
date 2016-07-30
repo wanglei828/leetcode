71448bb69ef5d91cd99652fe64fff0ffeb6bb7cf
Lei <bestwanglei@gmail.com>
2016-07-30 11:24:27 -0700

    Update Solution.java

--- algorithms/Wildcard-Matching_hard/Solution.java
+++ algorithms/Wildcard-Matching_hard/Solution.java
@@ -23,34 +23,29 @@ public class Solution {
     public boolean isMatch(String s, String p) {
         if(s == null && p == null) return true;
         if(s == null || p == null) return false;
-        int i=0;
-        int j=0;
-        int sIndex = -1;
-        int starIndex = -1;
-        while(i < s.length()) {
-            if(j < p.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
+        int lns = s.length(), lnp = p.length();
+        int i=0, j=0;
+        int jstar = -1;
+        int istar = -1;
+        while(i<lns) {
+            if(j<lnp && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')) {
                 i++;
                 j++;
-            } else if(j < p.length() && p.charAt(j) == '*') {
-                starIndex = j;
-                sIndex = i;
+            } else if(j<lnp && p.charAt(j) == '*') {
+                jstar = j;
+                istar = i;
                 j++;
-            } else if(starIndex != -1) {
-                j = starIndex + 1;
-                i = sIndex + 1;
-                sIndex++;
+            } else if(jstar != -1) {
+                j = jstar + 1;
+                i = istar + 1;
+                istar++;
             } else {
                 break;
             }
         }
-        if(i == s.length()) {
-            while(j<p.length()) {
-                if(p.charAt(j) != '*') {
-                    return false;
-                }
-                j++;
-            }
-            return true;
+        if(i == lns) {
+            while(j<lnp && p.charAt(j) == '*') j++;
+            return (j == lnp)? true : false;
         } else {
             return false;
         }
