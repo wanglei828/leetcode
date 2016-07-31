54ea4fc8300281eba19e518740ec5ced54296f65
Lei <bestwanglei@gmail.com>
2016-07-31 15:50:23 -0700

    Update Solution.java

--- algorithms/Letter-Combinations-of-a-Phone-Number_medium/Solution.java
+++ algorithms/Letter-Combinations-of-a-Phone-Number_medium/Solution.java
@@ -11,31 +11,23 @@ Although the above answer is in lexicographical order, your answer could be in a
 
 public class Solution {
     List<String> res = new ArrayList<String>();
+    private String[] nums = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
         if(digits == null || digits.length() == 0 || digits.contains("0") || digits.contains("1")) return new ArrayList<String>();
-        Map<Character, String> map = new HashMap<Character, String>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
-        helper(map, digits, 0, new StringBuilder());
+        helper(digits, 0, new StringBuilder());
         return res;
     }
-    private void helper(Map<Character, String> map, String digits, int start, StringBuilder sb) {
+    private void helper(String digits, int start, StringBuilder sb) {
         if(start == digits.length()) {
             res.add(sb.toString());
             return;
         }
         char c = digits.charAt(start);
-        String str = map.get(c);
+        String str = nums[c-'0'];
         int len = sb.length();
         for(int i=0; i<str.length(); i++) {
             sb.append(str.charAt(i));
-            helper(map, digits, start+1, sb);
+            helper(digits, start+1, sb);
             sb.setLength(len);
         }
     }
