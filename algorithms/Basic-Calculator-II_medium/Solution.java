3f400a16c9b970177ba92198e67cc5eb88bbd858
Lei <bestwanglei@gmail.com>
2016-08-03 14:42:44 -0700

    Update Solution.java

--- algorithms/Basic-Calculator-II_medium/Solution.java
+++ algorithms/Basic-Calculator-II_medium/Solution.java
@@ -13,39 +13,39 @@ Some examples:
 */
 
 public class Solution {
-	public int calculate(String s) {
-	    if(s == null || s.length() == 0) return 0;
-		String in = s.replaceAll("\\s", "") + " ";
-        Stack<Integer> stack = new Stack<Integer>();
-        char sign = '+';
-        int n = in.length();
-        int start = 0;
-        for(int i=0; i<n; i++) {
-            char c = in.charAt(i);
-            if(!Character.isDigit(c)) {
-                int num = Integer.valueOf(in.substring(start, i));
-                if(stack.isEmpty() || sign == '+' || sign == '-') {
-                   if(sign == '+') {
-                       stack.push(num);
-                   } else {
-                       stack.push(0-num);
-                   }
-                } else {
-                    int num1 = stack.pop();
-                    if(sign == '*') {
-                        stack.push(num*num1);
-                    } else if (sign == '/') {
-                        stack.push(num1/num);
-                    }
-                }
-                sign = c;
-                start = i+1;
-            }
-        }
+   public int calculate(String s) {
+	if(s == null || s.length() == 0) return 0;
+        s = s.replaceAll(" ", "") + '#';
+        char op = '+';
         int res = 0;
+        int start = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        int n = s.length();
+        for(int i=0; i<n; i++) {
+            char c = s.charAt(i);
+            if(c >= '0' && c <= '9') {
+                continue;
+            } else {
+                int cur = Integer.valueOf(s.substring(start,i));
+                switch(op) {
+                    case '+': 
+                    case '-':
+                        stack.push((op == '+')? cur : -cur);
+                        break;
+                    case '*':
+                        stack.push(stack.pop()*cur);
+                        break;
+                    case '/':
+                        stack.push(stack.pop()/cur);
+                        break;
+                }
+            }
+            op = c;
+            start = i+1;
+        }
         while(!stack.isEmpty()) {
             res += stack.pop();
         }
         return res;
-	}
+   }
 }
