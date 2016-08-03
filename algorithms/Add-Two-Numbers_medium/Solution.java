4505aff2a7321e4ef5709724894353c195424413
Lei <bestwanglei@gmail.com>
2016-08-03 15:48:14 -0700

    Update Solution.java

--- algorithms/Add-Two-Numbers_medium/Solution.java
+++ algorithms/Add-Two-Numbers_medium/Solution.java
@@ -13,35 +13,36 @@ Output: 7 -> 0 -> 8
  *     ListNode(int x) { val = x; }
  * }
  */
+ 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
         if(l2 == null) return l1;
-        ListNode res = new ListNode(-1);
-        res.next = l1;
+        ListNode res = new ListNode(0);
         int carry = 0;
-        ListNode last = null;
+        ListNode pre = res;
         while(l1 != null && l2 != null) {
             int sum = l1.val + l2.val + carry;
             carry = sum/10;
             l1.val = sum%10;
-            last = l1;
+            pre.next = l1;
+            pre = pre.next;
             l1 = l1.next;
             l2 = l2.next;
         }
         ListNode l = (l1 == null)? l2 : l1;
-        last.next = l;
         while(carry != 0 && l != null) {
             int sum = l.val + carry;
             l.val = sum%10;
             carry = sum/10;
-            last = l;
+            pre.next = l;
+            pre = pre.next;
             l = l.next;
         }
         if(carry == 1) {
-            ListNode tail = new ListNode(1);
-            last.next = tail;
-            tail.next = null;
+            pre.next = new ListNode(1);
+        } else {
+            pre.next = l;
         }
         return res.next;
     }
