61508d32983959c542a274f10a2215a72ffaf085
Lei Wang <bestwanglei@gmail.com>
2019-11-13 23:20:37 -0800

    update code.

--- algorithms/Generate-Parentheses_medium/Solution.java
+++ algorithms/Generate-Parentheses_medium/Solution.java
@@ -15,10 +15,8 @@ For example, given n = 3, a solution set is:
 public class Solution {
     List<String> res = new ArrayList<String>();
     public List<String> generateParenthesis(int n) {
-        if(n == 0) return res;
         StringBuilder sb = new StringBuilder();
-        sb.append("(");
-        generate(sb, 1, 0, n);
+        generate(sb, 0, 0, n);
         return res;
     }
     
@@ -27,16 +25,15 @@ public class Solution {
             res.add(sb.toString());
             return;
         }
-        int len = sb.length();
         if(left < n) {
-            sb.append("(");
-            generate(sb, left+1, right, n);
-            sb.setLength(len);
+            StringBuilder newsb = new StringBuilder(sb.toString());
+            newsb.append("(");
+            generate(newsb, left+1, right, n);
         }
         if(left > right) {
-            sb.append(")");
-            generate(sb, left, right+1, n);
-            sb.setLength(len);
+            StringBuilder newsb = new StringBuilder(sb.toString());
+            newsb.append(")");
+            generate(newsb, left, right+1, n);
         }
     }
 }
