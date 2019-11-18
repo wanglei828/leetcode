8a5ea85c156beec6d6acb79e3b61c86ad43a26b3
Lei Wang <bestwanglei@gmail.com>
2019-11-18 13:15:55 -0800

    Create solution.java

---
+++ algorithms/Sum_of_Left_Leaves-easy/solution.java
@@ -0,0 +1,37 @@
+/*
+
+Find the sum of all left leaves in a given binary tree.
+
+Example:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int sum = 0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left != null && root.left.right == null && root.left.left == null) {
+            sum += root.left.val;
+        }
+        if (root.left != null) sumOfLeftLeaves(root.left);
+        if (root.right != null) sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
