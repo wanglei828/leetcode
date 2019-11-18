61de3e374daa90d2107ce2382cab5f99d4645f94
Lei Wang <bestwanglei@gmail.com>
2019-11-18 12:19:10 -0800

    Create solution.java

---
+++ algorithms/Continuous_Subarray_Sum-medium/solution.java
@@ -0,0 +1,57 @@
+/*
+
+Given a list of non-negative numbers and a target integer k, 
+write a function to check if the array has a continuous subarray of size at least 2 that sums up to a multiple of k, 
+that is, sums up to n*k where n is also an integer.
+
+Example 1:
+
+Input: [23, 2, 4, 6, 7],  k=6
+Output: True
+Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+Example 2:
+
+Input: [23, 2, 6, 4, 7],  k=6
+Output: True
+Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+ 
+
+Note:
+
+The length of the array won't exceed 10,000.
+You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
+
+*/
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if(nums == null || nums.length < 2) return false;
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        if(k == 0) {
+            for (int i=0; i<n-1; i++) {
+                if(nums[i] + nums[i+1] == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        int sum = nums[0];
+        map.put(nums[0]%k, 0);
+        for(int i=1; i<n; i++) {
+            sum += nums[i];
+            int key = sum%k;
+            if (key == 0) {
+                return true;
+            }
+            if(map.containsKey(key)) {
+                if(i - map.get(key) > 1) {
+                    return true;
+                }
+            } else {
+                map.put(key, i);
+            }
+        }
+        return false;
+    }
+}
