b74150c37b90337d46e50fdf96c614434a15a220
Lei Wang <bestwanglei@gmail.com>
2019-12-31 21:59:15 -0800

    Create solution.java

---
+++ algorithms/Long_Pressed_Name-easy/solution.java
@@ -0,0 +1,62 @@
+/*
+
+Your friend is typing his name into a keyboard.  
+Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
+
+You examine the typed characters of the keyboard.  
+Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
+
+ 
+
+Example 1:
+
+Input: name = "alex", typed = "aaleex"
+Output: true
+Explanation: 'a' and 'e' in 'alex' were long pressed.
+Example 2:
+
+Input: name = "saeed", typed = "ssaaedd"
+Output: false
+Explanation: 'e' must have been pressed twice, but it wasn't in the typed output.
+Example 3:
+
+Input: name = "leelee", typed = "lleeelee"
+Output: true
+Example 4:
+
+Input: name = "laiden", typed = "laiden"
+Output: true
+Explanation: It's not necessary to long press any character.
+ 
+
+Note:
+
+name.length <= 1000
+typed.length <= 1000
+The characters of name and typed are lowercase letters.
+
+*/
+
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i =0, j=0;
+        while(i < name.length() && j < typed.length()) {
+            int cnt_n = 1;
+            while(i+1 < name.length() && name.charAt(i) == name.charAt(i+1)) {
+                cnt_n++;
+                i++;
+            }
+            int cnt_t = 1;
+            while(j+1 < typed.length() && typed.charAt(j) == typed.charAt(j+1)) {
+                cnt_t++;
+                j++;
+            }
+            if(name.charAt(i) != typed.charAt(j) || cnt_n > cnt_t) {
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return i == name.length() && j == typed.length();
+    }
+}
