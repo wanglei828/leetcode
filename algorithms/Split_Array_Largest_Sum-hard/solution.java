27f0fd723d7659e1db9e6eceaed87356d4f07834
Lei Wang <bestwanglei@gmail.com>
2020-01-03 21:24:55 -0800

    Create solution.java

---
+++ algorithms/Split_Array_Largest_Sum-hard/solution.java
@@ -0,0 +1,61 @@
+/*
+
+Given an array which consists of non-negative integers and an integer m, 
+you can split the array into m non-empty continuous subarrays. 
+Write an algorithm to minimize the largest sum among these m subarrays.
+
+Note:
+If n is the length of array, assume the following constraints are satisfied:
+
+1 ≤ n ≤ 1000
+1 ≤ m ≤ min(50, n)
+Examples:
+
+Input:
+nums = [7,2,5,10,8]
+m = 2
+
+Output:
+18
+
+Explanation:
+There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8],
+where the largest sum among the two subarrays is only 18.
+
+*/
+
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        long l = 0;
+        long r = 0;        
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            r += nums[i];
+            if (l < nums[i]) {
+                l = nums[i];
+            }
+        }
+        long ans = r;
+        while (l <= r) {
+            long mid = (l + r)/2;
+            long sum = 0;
+            int cnt = 1;
+            for (int i = 0; i < n; i++) {
+                if (sum + nums[i] > mid) {
+                    cnt++;
+                    sum = nums[i];
+                } else {
+                    sum += nums[i];
+                }
+            }
+            if (cnt <= m) {
+                ans = Math.min(ans, mid);
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)ans;      
+    }
+}
