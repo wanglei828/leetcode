1d759976ba79488b74ce3b3d35cc406554a81830
Lei Wang <bestwanglei@gmail.com>
2022-03-18 21:01:16 -0700

    Create solution.java

---
+++ algorithms/Asteroid_Collision-Medium/solution.java
@@ -0,0 +1,76 @@
+/*
+We are given an array asteroids of integers representing asteroids in a row.
+
+For each asteroid, the absolute value represents its size, 
+and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+
+Find out the state of the asteroids after all collisions. 
+If two asteroids meet, the smaller one will explode. 
+If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+
+ 
+
+Example 1:
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+Example 2:
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+Example 3:
+
+Input: asteroids = [10,2,-5]
+Output: [10]
+Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+ 
+
+Constraints:
+
+2 <= asteroids.length <= 104
+-1000 <= asteroids[i] <= 1000
+asteroids[i] != 0
+
+*/
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st = new Stack<>();
+        int n = asteroids.length;
+        for (int i = 0; i < n ; i++) {
+            int cur = asteroids[i];
+            if (st.isEmpty()) {
+                st.push(cur);
+            } else {
+                while (!st.isEmpty()) {
+                    if (st.peek() > 0 && cur <0) {
+                        if (Math.abs(st.peek()) > Math.abs(cur)) {
+                            cur = 0;
+                            break;
+                        } else if (Math.abs(st.peek()) < Math.abs(cur)){
+                            st.pop();
+                        } else {
+                            st.pop();
+                            cur = 0;
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+                if (cur != 0) {
+                    st.push(cur);
+                }
+            }
+        }
+        n = st.size();
+        int[] res = new int[n];
+        while(!st.isEmpty()) {
+            res[n-1] = st.pop();
+            n--;
+        }
+        return res;
+    }
+}
