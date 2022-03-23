d4a7145893a629e861f0cc16eba6d157e6a47efd
Lei Wang <bestwanglei@gmail.com>
2022-03-22 20:28:42 -0700

    Create solution.java

---
+++ algorithms/Design_A_Leaderboard-medium/solution.java
@@ -0,0 +1,73 @@
+/*
+Design a Leaderboard class, which has 3 functions:
+
+addScore(playerId, score): Update the leaderboard by adding score to the given player's score. 
+If there is no player with such id in the leaderboard, add him to the leaderboard with the given score.
+top(K): Return the score sum of the top K players.
+reset(playerId): Reset the score of the player with the given id to 0 (in other words erase it from the leaderboard). 
+It is guaranteed that the player was added to the leaderboard before calling this function.
+Initially, the leaderboard is empty.
+
+ 
+
+Example 1:
+
+Input: 
+["Leaderboard","addScore","addScore","addScore","addScore","addScore","top","reset","reset","addScore","top"]
+[[],[1,73],[2,56],[3,39],[4,51],[5,4],[1],[1],[2],[2,51],[3]]
+Output: 
+[null,null,null,null,null,null,73,null,null,null,141]
+
+Explanation: 
+Leaderboard leaderboard = new Leaderboard ();
+leaderboard.addScore(1,73);   // leaderboard = [[1,73]];
+leaderboard.addScore(2,56);   // leaderboard = [[1,73],[2,56]];
+leaderboard.addScore(3,39);   // leaderboard = [[1,73],[2,56],[3,39]];
+leaderboard.addScore(4,51);   // leaderboard = [[1,73],[2,56],[3,39],[4,51]];
+leaderboard.addScore(5,4);    // leaderboard = [[1,73],[2,56],[3,39],[4,51],[5,4]];
+leaderboard.top(1);           // returns 73;
+leaderboard.reset(1);         // leaderboard = [[2,56],[3,39],[4,51],[5,4]];
+leaderboard.reset(2);         // leaderboard = [[3,39],[4,51],[5,4]];
+leaderboard.addScore(2,51);   // leaderboard = [[2,51],[3,39],[4,51],[5,4]];
+leaderboard.top(3);           // returns 141 = 51 + 51 + 39;
+*/
+
+class Leaderboard {
+    HashMap<Integer, Integer> map;
+    public Leaderboard() {
+        map = new HashMap<>();
+    }
+    
+    public void addScore(int playerId, int score) {
+        if (map.containsKey(playerId)) {
+            map.put(playerId, map.get(playerId) + score);
+        } else {
+            map.put(playerId, score); 
+        }
+        
+    }
+    
+    public int top(int K) {
+        PriorityQueue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>((a, b)->{ return b.getValue() - a.getValue();});
+        for (Map.Entry<Integer,Integer> e : map.entrySet()) {
+            q.add(e);
+        }
+        int sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += q.poll().getValue();
+        }
+        return sum;
+    }
+    
+    public void reset(int playerId) {
+        map.put(playerId, 0);
+    }
+}
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard obj = new Leaderboard();
+ * obj.addScore(playerId,score);
+ * int param_2 = obj.top(K);
+ * obj.reset(playerId);
+ */
