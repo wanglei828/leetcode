4ce728e7b9ba839bed2588a4ee51b193d8ca3fdc
Lei Wang <bestwanglei@gmail.com>
2022-04-02 21:57:27 -0700

    Update Solution.java

--- algorithms/Set-Matrix-Zeroes_medium/Solution.java
+++ algorithms/Set-Matrix-Zeroes_medium/Solution.java
@@ -6,31 +6,23 @@ public class Solution {
     public void setZeroes(int[][] matrix) {
         if(matrix.length == 0) return;
         if(matrix[0].length == 0) return;
-        int r = matrix.length;
-        int c = matrix[0].length;
-        int[] rows = new int[r];
-        int[] cols = new int[c];
-        Arrays.fill(rows, 0);
-        Arrays.fill(cols, 0);
-        for(int i = 0; i <r; i++) {
-            for(int j=0; j<c; j++) {
-                if(matrix[i][j] == 0) {
-                    rows[i] = 1;
-                    cols[j] = 1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
                 }
             }
         }
-        for(int i = 0; i<r; i++){
-            if(rows[i] == 1) {
-                Arrays.fill(matrix[i], 0);
-            }
-        }
-        for(int i=0; i<c; i++) {
-            if(cols[i] == 1) {
-                for(int j=0; j<r; j++) {
-                    matrix[j][i] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
                 }
             }
         }
-    }
 }
