b2bfdad15381c4049fa76ee0ddf70fb3819594fb
Lei Wang <bestwanglei@gmail.com>
2022-04-03 11:35:17 -0700

    Create solution.java

---
+++ algorithms/Valid_Parenthesis_String-medium/solution.java
@@ -0,0 +1,53 @@
+/*
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'.
+*/
+
+class Solution {
+    public boolean checkValidString(String s) {
+        // min, max is the cnt of '('
+        int min = 0, max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ( c == '(') {
+                min++;
+                max++;
+            } else if (c == ')') {
+                min--;
+                max--;
+            } else {
+                min--;
+                max++;
+            }
+            if (max < 0) return false;
+            min = Math.max(0, min);
+        }
+        return min == 0;
+    }
+}
