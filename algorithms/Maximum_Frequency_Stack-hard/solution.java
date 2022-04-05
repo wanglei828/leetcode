3d0bc33b00571394ee11623e4403a77a51d7798d
Lei Wang <bestwanglei@gmail.com>
2022-04-04 19:22:45 -0700

    Create solution.java

---
+++ algorithms/Maximum_Frequency_Stack-hard/solution.java
@@ -0,0 +1,83 @@
+/*
+Design a stack-like data structure to push elements to the stack and pop the most frequent element from the stack.
+
+Implement the FreqStack class:
+
+FreqStack() constructs an empty frequency stack.
+void push(int val) pushes an integer val onto the top of the stack.
+int pop() removes and returns the most frequent element in the stack.
+If there is a tie for the most frequent element, the element closest to the stack's top is removed and returned.
+ 
+
+Example 1:
+
+Input
+["FreqStack", "push", "push", "push", "push", "push", "push", "pop", "pop", "pop", "pop"]
+[[], [5], [7], [5], [7], [4], [5], [], [], [], []]
+Output
+[null, null, null, null, null, null, null, 5, 7, 5, 4]
+
+Explanation
+FreqStack freqStack = new FreqStack();
+freqStack.push(5); // The stack is [5]
+freqStack.push(7); // The stack is [5,7]
+freqStack.push(5); // The stack is [5,7,5]
+freqStack.push(7); // The stack is [5,7,5,7]
+freqStack.push(4); // The stack is [5,7,5,7,4]
+freqStack.push(5); // The stack is [5,7,5,7,4,5]
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,5,7,4].
+freqStack.pop();   // return 7, as 5 and 7 is the most frequent, but 7 is closest to the top. The stack becomes [5,7,5,4].
+freqStack.pop();   // return 5, as 5 is the most frequent. The stack becomes [5,7,4].
+freqStack.pop();   // return 4, as 4, 5 and 7 is the most frequent, but 4 is closest to the top. The stack becomes [5,7].
+ 
+
+Constraints:
+
+0 <= val <= 109
+At most 2 * 104 calls will be made to push and pop.
+It is guaranteed that there will be at least one element in the stack before calling pop.
+*/
+
+class FreqStack {
+    Map<Integer, Integer> freq;
+    Map<Integer, Stack<Integer>> fmap;
+    int max;
+    public FreqStack() {
+        freq = new HashMap<>();
+        fmap = new HashMap<>();
+        max = 0;
+    }
+    
+    public void push(int val) {
+        if (freq.containsKey(val)) {
+            freq.put(val, freq.get(val) + 1);
+        } else {
+            freq.put(val, 1);
+        }
+        int cur = freq.get(val);
+        if (max < cur) {
+            max = cur;
+        }
+        if (!fmap.containsKey(cur)) {
+            fmap.put(cur, new Stack<Integer>());
+        }
+        fmap.get(cur).push(val);
+    }
+    
+    public int pop() {
+        int res = fmap.get(max).pop();
+        freq.put(res, freq.get(res) - 1);
+        if (fmap.get(max).size() == 0) {
+            fmap.remove(max);
+            max--;
+        }
+        return res;
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack obj = new FreqStack();
+ * obj.push(val);
+ * int param_2 = obj.pop();
+ */
