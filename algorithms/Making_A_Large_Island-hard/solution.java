6b3e0641cd465e07c2b1e038e6df440a79a90de6
Lei Wang <bestwanglei@gmail.com>
2022-04-06 15:14:31 -0700

    Update solution.java

--- algorithms/Making_A_Large_Island-hard/solution.java
+++ algorithms/Making_A_Large_Island-hard/solution.java
@@ -34,76 +34,66 @@ grid[i][j] is either 0 or 1.
 
 class Solution {
     int m, n;
-    boolean[][] visit;
-    List<Set<int[]>> islands;
     int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+    Map<Integer, Integer> area;
     public int largestIsland(int[][] grid) {
         m = grid.length;
         n = grid[0].length;
-        visit = new boolean[m][n];
-        islands = new ArrayList<>();
+        area = new HashMap<>();
+        int id = 2;
         int res = 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0 || visit[i][j]) {
+                if (grid[i][j] != 1) {
                     continue;
                 }
-                Set<int[]> set = new HashSet<>();
-                dfs(grid, i, j, set);
-                islands.add(set);
-                res = Math.max(res, set.size());
+                int[] cnt = new int[1];
+                dfs(grid, i, j, id, cnt);
+                res = Math.max(res, cnt[0]);
+                area.put(id, cnt[0]);
+                id++;
             }
         }
-        if (islands.size() == 1 && res < m*n) {
-            return res + 1;
-        }
-        List<Set<Pair<Integer, Integer>>> move = new ArrayList<>();
-        for (int i = 0; i < islands.size(); i++) {
-            Set<int[]> si = islands.get(i);
-            Set<Pair<Integer, Integer>> pi = new HashSet<>();
-            getMove(si, pi, grid);
-            move.add(pi);
-        }
-        Set<Pair<Integer, Integer>> visit = new HashSet<>();
-        for (int i = 0; i < move.size(); i++) {
-            for (Pair<Integer, Integer> p : move.get(i)) {
-                if (visit.contains(p)) continue;
-                visit.add(p);
-                int area = islands.get(i).size();
-                for (int j = i+1; j < move.size(); j++) {
-                    if (move.get(j).contains(p)) {
-                        area += islands.get(j).size();
-                    }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    int cur = count(grid, i, j);
+                    res = Math.max(res, cur);
                 }
-                res = Math.max(res, area + 1);
             }
-            
         }
         return res;
     }
     
-    private void dfs(int[][] grid, int x, int y, Set<int[]> set) {
-        visit[x][y] = true;
-        set.add(new int[]{x,y});
+    private void dfs(int[][] grid, int x, int y, int id, int[] cnt) {
+        cnt[0]++;
+        grid[x][y] = id;
         for (int k = 0; k < 4; k++) {
             int[] dir = dirs[k];
             int nx = x + dir[0];
             int ny = y + dir[1];
             if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-            if (visit[nx][ny]) continue;
-            if (grid[nx][ny] == 0) continue;
-            dfs(grid, nx, ny, set);
+            if (grid[nx][ny] != 1) continue;
+            dfs(grid, nx, ny, id, cnt);
         }
     }
     
-    private void getMove(Set<int[]> si, Set<Pair<Integer, Integer>> pi, int[][] grid) {
-        for (int[] p: si) {
-            for (int k = 0; k < 4; k++) {
-                int nx = p[0] + dirs[k][0];
-                int ny = p[1] + dirs[k][1];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n || grid[nx][ny] == 1) continue;
-                pi.add(new Pair<Integer, Integer>(nx, ny));
+    private int count(int[][] grid, int x, int y) {
+        Set<Integer> set = new HashSet<>();
+        for (int k = 0; k < 4; k++) {
+            int[] dir = dirs[k];
+            int nx = x + dir[0];
+            int ny = y + dir[1];
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+            if (grid[nx][ny] > 1) {
+                set.add(grid[nx][ny]);
             }
         }
+        int res = 1;
+        for (int id : set) {
+            res += area.get(id);
+        }
+        return res;
     }
 }
