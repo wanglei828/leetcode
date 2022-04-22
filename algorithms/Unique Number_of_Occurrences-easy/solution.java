8936901247b0d93bb58f59b0feca8580b7860bc5
Lei Wang <bestwanglei@gmail.com>
2022-04-21 18:17:33 -0700

    Create solution.java

---
+++ algorithms/Unique Number_of_Occurrences-easy/solution.java
@@ -0,0 +1,45 @@
+/*
+Given an array of integers arr, return true if the number of occurrences of each value in the array is unique, or false otherwise.
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+*/
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for (int a : arr) {
+            if (map.containsKey(a)) {
+                map.put(a, map.get(a) + 1);
+            } else {
+                map.put(a, 1);
+            }
+        }
+        for (int k : map.keySet()) {
+            int freq = map.get(k);
+            if (set.contains(freq)) {
+                return false;
+            }
+            set.add(freq);
+        }
+        return true;
+    }
+}
