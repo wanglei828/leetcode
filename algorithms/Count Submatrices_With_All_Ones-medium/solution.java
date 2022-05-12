9c391bc9a8accfc8e3f4e75c87d6587fef992084
Lei Wang <bestwanglei@gmail.com>
2022-05-11 23:22:58 -0700

    Create solution.java

---
+++ algorithms/Count Submatrices_With_All_Ones-medium/solution.java
@@ -0,0 +1,67 @@
+/*
+Given an m x n binary matrix mat, return the number of submatrices that have all ones.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[1,0,1],[1,1,0],[1,1,0]]
+Output: 13
+Explanation: 
+There are 6 rectangles of side 1x1.
+There are 2 rectangles of side 1x2.
+There are 3 rectangles of side 2x1.
+There is 1 rectangle of side 2x2. 
+There is 1 rectangle of side 3x1.
+Total number of rectangles = 6 + 2 + 3 + 1 + 1 = 13.
+Example 2:
+
+
+Input: mat = [[0,1,1,0],[0,1,1,1],[1,1,1,0]]
+Output: 24
+Explanation: 
+There are 8 rectangles of side 1x1.
+There are 5 rectangles of side 1x2.
+There are 2 rectangles of side 1x3. 
+There are 4 rectangles of side 2x1.
+There are 2 rectangles of side 2x2. 
+There are 2 rectangles of side 3x1. 
+There is 1 rectangle of side 3x2. 
+Total number of rectangles = 8 + 5 + 2 + 4 + 2 + 2 + 1 = 24.
+ 
+
+Constraints:
+
+1 <= m, n <= 150
+mat[i][j] is either 0 or 1.
+*/
+
+class Solution {
+    public int numSubmat(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res += find(mat, i, j);
+            }
+        }
+        return res;
+    }
+    
+    private int find(int[][]mat, int x, int y) {
+        int cnt = 0;
+        int col = mat[0].length;
+        for (int i = x; i < mat.length; i++) {
+            for (int j = y; j < col; j++) {
+                if (mat[i][j] == 1) {
+                    cnt++;
+                } else {
+                    col = j;
+                }
+            }
+        }
+        return cnt;
+    }
+}
