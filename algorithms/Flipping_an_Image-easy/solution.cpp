35cab1f9906682658bad088d326a39f88c699832
Lei Wang <bestwanglei@gmail.com>
2022-05-15 11:21:26 -0700

    Create solution.cpp

---
+++ algorithms/Flipping_an_Image-easy/solution.cpp
@@ -0,0 +1,53 @@
+/*
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+To flip an image horizontally means that each row of the image is reversed.
+
+For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+For example, inverting [0,1,1] results in [1,0,0].
+ 
+
+Example 1:
+
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+Example 2:
+
+Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ 
+
+Constraints:
+
+n == image.length
+n == image[i].length
+1 <= n <= 20
+images[i][j] is either 0 or 1.
+*/
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int m = image.size();
+        int n = image[0].size();
+        for (int i = 0; i < m; i++) {
+            int h = 0;
+            int t = n - 1;
+            while (h <= t) {
+                int vh = image[i][h] ^ 1;
+                int vt = image[i][t] ^ 1;
+                image[i][h] = vt;
+                image[i][t] = vh;
+                h++;
+                t--;
+            }
+        }
+        return image;
+    }
+};
